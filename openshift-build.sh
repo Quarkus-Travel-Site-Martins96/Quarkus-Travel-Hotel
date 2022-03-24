ea6fdb7da3fcf82a814205209b0d60163b670d07
Martins96 <luca.martinelli7cc@gmail.com>
2022-03-25 00:25:06 +0100

    Rebuilded and update Angular configuration

--- openshift-build.sh
+++ openshift-build.sh
@@ -2,4 +2,4 @@ cd src/main/angular/hotel-ng
 npm i
 ng build --prod
 cd ../../../..
-./mvnw clean package -Dquarkus.kubernetes.deploy=true -DskipTests
+./mvnw clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.kubernetes.deployment-target=openshift -Dquarkus.openshift.expose=true -DskipTests
